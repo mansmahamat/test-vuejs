2500879257d8423fe8a3dd556da0b961efd2a096
mansmahamat <mansour.mahamat.salle@gmail.com>
2020-09-01 01:31:39 +0200

    Add loading more quote, display all characters

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-unresolved
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
--- src/views/Personnages.vue
+++ src/views/Personnages.vue
@@ -7,61 +7,89 @@ export default {
   data() {
     return {
       pending: false,
+      loading: false,
+      charactersInfo: [],
+      quoteInfo: [],
+      errorQuote: null,
+      error: null,
+      isOpen: false,
     };
   },
   mounted() {
-    // API request using axios to restcountries.eu
+    // API request
     this.pending = true;
     axios
       .get('https://www.breakingbadapi.com/api/characters')
-      .then((response) => { (this.charactersInfo = response.data); })
-      .catch((error) => { (this.error = error); })
+      .then((response) => { (this.charactersInfo = response.data); console.log(response.data); })
+      .catch((error) => { (this.error = error); console.log(error); })
       .finally(() => { this.pending = false; });
+    this.loading = true;
+    axios
+      .get('https://www.breakingbadapi.com/api/quote?author=Walter+White')
+      .then((response) => { (this.quoteInfo = response.data); console.log(response.data); })
+      .catch((error) => { (this.errorQuote = error); console.log(error); })
+      .finally(() => { this.loading = false; });
   },
 };
 </script>
 <template>
+
   <div class="container mx-auto px-4">
-    <h1>This is an PERSONAGE page page</h1>
+    <h1>Liste complète des personnages</h1>
+    <section class="my-8 border-2 border-black rounded-lg py-4 px-2 bg-green-700 text-white"
+             v-if="error">
+      <p>
+        Nous sommes désolés, nous ne sommes pas en mesure de récupérer ces informations.
+        Veuillez réessayer ultérieurement.
+      </p>
+    </section>
     <div class="loaderFlex"><div v-if="pending" class="loader"></div></div>
-    <div class="p-4 md:w-1/3">
-        <div class="h-full border-2 border-gray-200 rounded-lg overflow-hidden">
-          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402" alt="blog">
-          <div class="p-6">
-            <h2 class="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
-              CATEGORY
-            </h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
-            <p class="leading-relaxed mb-3">
-              Photo booth fam kinfolk ngs jianbing microdosing tousled waistcoat.</p>
-            <div class="flex items-center flex-wrap ">
-              <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
-                <svg class="w-4 h-4 ml-2"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                  <path d="M5 12h14"></path>
-                  <path d="M12 5l7 7-7 7"></path>
-                </svg>
+      <section class="text-gray-700 body-font">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="flex flex-wrap -m-4">
+            <div v-for="character in charactersInfo"
+                  :key="character.char_id" class="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <a class="block relative h-48 rounded overflow-hidden">
+                <img v-bind:alt="character.name"
+                      class="object-contain object-center w-full h-full block"
+                      v-bind:src="character.img" >
               </a>
-              <span class="text-gray-600 inline-flex items-center leading-none text-sm">
-                <svg class="w-4 h-4 mr-1"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round" viewBox="0 0 24 24">
-                </svg>6
-              </span>
+              <div class="mt-4 text-center">
+                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    Saison : {{ character.appearance }}
+                </h3>
+                <h2 class="text-gray-900 title-font text-lg font-medium">{{ character.name }}</h2>
+                <p class="mt-1">{{ character.nickname }}</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    <ul>
-       <li v-for="character in charactersInfo" :key="character.char_id">
-        {{ character.name }}
-      </li>
-    </ul>
+
+        <button @click="isOpen = !isOpen"
+                  class="flex mx-auto  text-white bg-green-700 border-0 py-2 px-8 rounded text-lg">
+                    Charger des citations de Walter White
+              <i :class=" isOpen ? 'fas fa-arrow-up ml-4 mt-2': 'fas fa-arrow-down ml-4 mt-2'"></i>
+          </button>
+      </section>
+      <div :class="isOpen ? 'flex': 'hidden'"
+            class="  flex-wrap lg:w-4/5 mt-6 sm:mx-auto sm:mb-2 -mx-2">
+        <div class="loaderFlex"><div v-if="loading" class="loader"></div></div>
+          <div v-for="quote in quoteInfo"
+                :key="quote.quote_id" class="p-2 sm:w-1/2 w-full">
+            <div class="bg-gray-200 rounded flex p-4 h-full items-center">
+                <svg fill="none" stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="3"
+                    class="text-green-700 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                  <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                  <path d="M22 4L12 14.01l-3-3"></path>
+                </svg>
+                <span class="title-font font-medium">{{ quote.quote }}</span>
+              </div>
+            </div>
+
+    </div>
   </div>
 
 </template>
